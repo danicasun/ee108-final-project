30a2103d90a3834dd2f1f953a0ec8d20c8a67a0e
Taylor Dosia Tam <taylor@taylortam.com>
2026-03-11 14:49:00 -0700

    working on harmonics

--- src/design/harmonics.v
+++ src/design/harmonics.v
@@ -1,6 +1,7 @@
 module harmonics #(
-    parameter PHASE_W = 24, 
-    parameter SAMPLE_W = 16
+    parameter PHASE_W = 20, 
+    parameter SAMPLE_W = 16,
+    parameter W_SHIFT = 5
 )(
     input clk, 
     input reset, 
@@ -12,5 +13,18 @@ module harmonics #(
     output reg sample_ready 
 );
 
+//harmonic step sizes
+wire [PHASE_W-1:0] step1 = step_size;
+wire [PHASE_W-1:0] step2 = step_size << 1;
+wire [PHASE_W-1:0] step3 = step_size + (step_size << 1);
+wire [PHASE_W-1:0] step4 = step_size << 2;
 
-endmodule;
+//instantiate sine readers for each harmonic
+
+
+//weights based on meta
+
+//sum
+
+
+endmodule
